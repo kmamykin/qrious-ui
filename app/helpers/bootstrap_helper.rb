cfca235dfb88ab74c12d2edb7cbc6eed55aadc75
Kliment Mamykin <kmamyk@yahoo.com>
2013-08-31 03:53:49 -0400

    Added icon_tag and status_label_tag

--- app/helpers/bootstrap_helper.rb
+++ app/helpers/bootstrap_helper.rb
@@ -1,5 +1,20 @@
 module BootstrapHelper
-  def bootstrap_helper
-    "BootstrapHelper 123 !".html_safe
+  # To build string either use [tag1, tag2].join("\n").html_safe
+  # or
+  # body = "".html_safe
+  # body.safe_concat content_tag ...
+  # body
+
+  def icon_tag(icon, spin: false)
+    icon.present? ? content_tag(:i, '', class: ['icon', "icon-#{icon}", spin && "icon-spin"]) : ''.html_safe
+  end
+
+  def status_label_tag(status, label: 'default', icon: nil, tooltip: '')
+    content_tag :span, class: ['label', "label-#{label.to_s}"], data: {toggle: 'tooltip'}, title: tooltip.to_s do
+      [
+          icon_tag(icon),
+          status.to_s.titleize
+      ].compact.join("\n").html_safe
+    end
   end
 end
--- vendor/assets/javascripts/qrious-ui.js.coffee
+++ vendor/assets/javascripts/qrious-ui.js.coffee
@@ -2,3 +2,7 @@
 #= require jquery/jquery
 #= require bootstrap/dist/js/bootstrap
 #= require_self
+
+$ ()->
+  # Enable Tooltips
+  $("*[data-toggle='tooltip']").tooltip()
